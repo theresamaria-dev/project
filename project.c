eaf6f793c768f9f6333877f15db199c43b19701c
theresamaria-dev <ghantoustheresamaria@gmail.com>
2025-10-03 22:09:13 +0300

    Create project.c

---
+++ project.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int verifyH(char** array, int rows, int cols, char c);
+int verifyV(char** array, int rows, int cols, char c);
+int verifyDown(char** array, int rows, int cols, char c);
+int verifyUp(char** array, int rows, int cols, char c);
+
+
+int verifyH(char** array, int rows, int cols, char c) {
+    for(int i=0; i<rows; i++) {
+      for(int j=0; j<cols; j++) {
+        if(array[i][j]==c && array[i][j+1]==c && array[i][j+2]==c && array[i][j+3]==c) {
+          return 1;
+        }
+      }
+    }
+    return 0;
+}
+
+int verifyV(char** array, int rows, int cols, char c) {
+    for(int i=0; i<rows; i++) {
+      for(int j=0; j<cols; j++) {
+        if(array[i][j]==c && array[i+1][j]==c && array[i+2][j]==c && array[i+3][j]==c){
+          return 1;
+        }
+      }
+    }
+    return 0;
+}
+ 
+int verifyDown(char** array, int rows, int cols, char c) { 
+    for(int i=0; i<rows-3; i++) {
+      for(int j=0; j<cols-3; j++) {
+        if(array[i][j]==c && array[i+1][j+1]==c && array[i+2][j+2]==c && array[i+3][j+3]==c) {
+          return 1;
+        }
+      }
+    }
+    return 0;
+}
+
+int verifyUp(char** array, int rows, int cols, char c) {
+    for(int i=6; i>=rows-3; i--) {
+      for(int j=0; j<cols-3; j++) {
+        if(array[i][j]==c && array[i-1][j+1]==c && array[i-2][j+2]==c && array[i-3][j+3]==c) {
+          return 1;
+        }
+      }
+    }
+    return 0;
+}
+
+
+int verify(char** array, char c, int rows, int cols) { //verify if a player won
+  if(verifyH(array, rows, cols, c) || verifyV(array, rows, cols, c) || verifyDown(array, rows, cols, c) || verifyUp(array, rows, cols, c)) {
+    return 1;
+  }
+  return 0;
+}
+
+
+void replace(char** array, int x, char c, int rows) { // replace the dot with A or B
+  x=x-1;
+  for (int i=rows-1; i>=0; i--) {
+    if (array[i][x] == '.') {
+      array[i][x] = c;
+      break;
+    }
+  }
+}
+
+void print(char** array, int rows, int cols) { // print the grid
+  for(int i=0; i<rows; i++) {
+    for(int j=0; j<cols; j++) {
+      printf("%c ", array[i][j]);
+    }
+    printf("\n");
+  }
+   printf("1 2 3 4 5 6 7");
+}
+
+int main() {
+  const int rows = 6;
+  const int cols = 7;
+   int count =0;
+  
+  printf("Welcome to Connect Four\n");
+  char space;
+  printf("Player A: ");
+  char c;
+  scanf("%c", &c);
+  scanf("%c",&space);
+  
+  printf("Player B: ");
+  char c2;
+  scanf("%c", &c2);
+  
+  char** array;
+  array = (char**) malloc(rows*sizeof(char*));
+  for(int i=0; i<=rows; i++) {
+    array[i] = (char*) malloc(cols*sizeof(char));
+    for(int j=0; j<=cols; j++) {
+      array[i][j] = '.';
+    }
+  }
+  
+  print(array, rows, cols);
+  
+  
+  
+  while(!(verify(array, c2, rows, cols)) && !(verify(array, c, rows, cols)) && count<=rows*cols) {
+    printf("\nPlayer A, choose a column(1-7): ");
+    int x;
+    scanf("%d", &x);
+    replace (array, x, c, rows);
+    print(array, rows, cols);
+    count++;
+    
+    if(!(verify(array, c2, rows, cols)) && !(verify(array, c, rows, cols))) {
+      printf("\nPlayer B, choose a column(1-7): ");
+      int y;
+      scanf("%d",&y);
+      replace (array, y, c2, rows);
+      print(array, rows, cols);
+      count++;
+    }
+  }
+  printf("%d\n",count);
+  
+  if(verify(array, c, rows, cols)) {
+    printf("\nPlayer A wins!");
+  }
+  else {
+    printf("\nPlayer B wins!");
+  }
+  
+}
