58540e1fdd3316b932e3f6507d00508ca704ebcf
fk-orb <farahkhalil2006@gmail.com>
2025-10-20 12:10:18 +0300

    Create game.h

---
+++ game.h
@@ -0,0 +1,12 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include<stdio.h>
+int verifyH(char** array, int rows, int cols, char c);
+int verifyV(char** array, int rows, int cols, char c);
+int verifyDown(char** array, int rows, int cols, char c);
+int verifyUp(char** array, int rows, int cols, char c);
+
+
+#endif
+
