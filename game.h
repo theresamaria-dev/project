a5b94ff5630f3980cec355f9babe0c3bf1a7a6d3
theresamaria-dev <ghantoustheresamaria@gmail.com>
2025-11-22 19:59:03 +0200

    Update game.h

--- game.h
+++ game.h
@@ -10,8 +10,9 @@ int verify(char** array, char c, int rows, int cols);
 int easyBot(char** array);
 int mediumBot(char** array, int rows, int cols);
 int hardBot(char** array, int rows, int cols);
-
-
+void replace(char** array, int x, char c, int rows);
+void print(char** array, int rows, int cols);
+int inRange(int a,int min,int max);
 
 #endif
 
