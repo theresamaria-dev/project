16b2d9f9f2e860326d325242a1e526461189f645
theresamaria-dev <ghantoustheresamaria@gmail.com>
2025-11-22 17:37:16 +0200

    Update main.c

--- main.c
+++ main.c
@@ -12,13 +12,8 @@ int main() {
 
     while (1) {
         printf("Would you like to play against human or bot? (1 for human, 2 for bot): ");
-        if (scanf("%d", &choice) != 1) {
+        if (scanf("%d", &choice) != 1 || (choice != 1 && choice != 2)) {
             printf("Invalid input. Please enter 1 or 2.\n");
-            while (getchar() != '\n');
-            continue;
-        }
-        if (choice != 1 && choice != 2) {
-            printf("Invalid choice. Please enter 1 or 2.\n");
             while (getchar() != '\n');
             continue;
         }
@@ -34,12 +29,12 @@ int main() {
                 while (getchar() != '\n');
                 continue;
             }
-            if (mode != 'E' && mode != 'H' && mode != 'e' && mode != 'h') {
-                printf("Invalid mode. Please enter E or H.\n");
+            mode = toupper(mode); //convert character to uppercase
+            if (mode != 'E' && mode != 'M' && mode != 'H') {
+                printf("Invalid mode. Please enter E, M, or H.\n");
                 while (getchar() != '\n');
                 continue;
             }
-            mode = (mode == 'h' ? 'H' : (mode == 'e' ? 'E' : mode));
             break;
         }
         while (getchar() != '\n');
@@ -95,14 +90,10 @@ int main() {
         printf("\nPlayer A, choose a column(1-7): \n");
         int x;
         while (1) { // if the user enters invalid input
-            if (scanf("%d", &x) != 1) {
-                printf("Invalid input, enter a number.\n");
+            if (scanf("%d", &x) != 1 || !inRange(x, 1, 7) || array[0][x - 1] != '.') {
+                printf("Invalid input, choose again.\n");
                 while (getchar() != '\n'); 
                 continue;  
-            }
-            if (!inRange(x, 1, 7) || array[0][x - 1] != '.') {
-                printf("Invalid column, choose again.\n");
-                continue;
             }
             break;
         }
@@ -113,17 +104,13 @@ int main() {
 
         // if player A and B did not win yet, ask player B again to enter a column
         int y;
-        if (!(verify(array, c2, rows, cols)) && !(verify(array, c, rows, cols))) {
+        if (!verify(array, c2, rows, cols) && !verify(array, c, rows, cols) && count <= rows*cols) {
             printf("\nPlayer B, choose a column(1-7): \n");
             if (choice == 1) {
                 while (1) {
-                    if (scanf("%d", &y) != 1) {
-                        printf("Invalid input, enter a number.\n");
+                    if (scanf("%d", &y) != 1 || !inRange(y, 1, 7) || array[0][y - 1] != '.') {
+                        printf("Invalid input, choose again.\n");
                         while (getchar() != '\n'); 
-                        continue;
-                    }
-                    if (!inRange(y, 1, 7) || array[0][y - 1] != '.') {
-                        printf("Invalid column, choose again.\n");
                         continue;
                     }
                     break;
@@ -135,7 +122,7 @@ int main() {
                     y = mediumBot(array, rows, cols);
                 } 
                 else {
-                    y = hardBot();
+                    y = hardBot(array, rows, cols);
                 }
             }
               
@@ -148,8 +135,11 @@ int main() {
     // check if a player won
     if (verify(array, c, rows, cols)) {
         printf("\nPlayer A wins!");
-    } else {
+    } else if (verify(array, c2, rows, cols)){
         printf("\nPlayer B wins!");
+    }
+    else {
+        printf("\nIt's a draw!\n");
     }
 
     for (int i = 0; i < rows; i++) {
