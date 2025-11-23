a392c038fb3a2f59e954db1d3cb7ef21361f7e83
rahmabezzaouia <rahmabezzaouia05@gmail.com>
2025-11-23 12:02:38 +0200

    Update game.h

--- game.h
+++ game.h
@@ -10,11 +10,11 @@ int verify(char** array, char c, int rows, int cols);
 int easyBot(char** array);
 int mediumBot(char** array, int rows, int cols);
 int findWinningMove(char** array, int rows, int cols, char player);
+int isBoardFull(char** board, int rows, int cols);
 int hardBot(char** array, int rows, int cols);
 void replace(char** array, int x, char c, int rows);
 void print(char** array, int rows, int cols);
 int inRange(int a,int min,int max);
-int isBoardFull(char** board, int rows, int cols);
 
 
 #endif
